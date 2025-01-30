41fb05f5d9d5d802f6da8d88d83746ed2e74b677
Jada Brown <jbrowbb@siue.edu>
2025-01-30 12:21:32 -0600

    adding data

--- hw2.asm
+++ hw2.asm
@@ -1,3 +1,14 @@
 # Jada Brown    800743234   jbrowbb@siue.edu ###########################
 # HW_2  allocate array and bubble sort #################################
 
+.data
+
+START_TEXT: .asciiz "The Original Array: "
+LIST_SEG: .asciiz ", "
+NEW_LINE: asciiz "\n"
+
+NEXT_TEXT: .asciiz "Sort Iterations:"
+LOOP_NUM: .asciiz "Iteration number "
+FILL: .asciiz ": "
+
+FINAL_TEXT: .asciiz "The Sorted Array: "
